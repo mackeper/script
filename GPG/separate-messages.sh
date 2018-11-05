6b6daa897c4967f4e2345ca5920313193a88c336
Marcus Östling <mpt.ostling@gmail.com>
2018-11-05 12:25:09 +0100

    add script to separate gpg messages

---
+++ GPG/separate-messages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file_count=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	if [ "============= separator =============" == "$line" ]
+	then
+		file_count=$((file_count+1))
+	else
+		echo $line >> "res_file$file_count"
+	fi
+done < "$1"
+file_count=$((file_count+1))
+echo "$file_count created."
